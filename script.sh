286bdbe2c769f47aada014c185409e37bb9399d6
bala15ozone <balajayaram22@gmail.com>
2019-12-13 00:04:03 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,5 +7,4 @@ sudo apt-get update -y && sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update && sudo apt-get install ansible -y
 ansible-playbook /tmp/provision.yml
-
 exit
